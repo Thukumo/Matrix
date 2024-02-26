bc865437ba475341f874ced34526e8d495f72249
つくも <92912896+Thukumo@users.noreply.github.com>
2024-02-26 10:25:51 +0900

    カラー出力時の縦ブレを完全になくした

--- matrix.py
+++ matrix.py
@@ -16,7 +16,8 @@ def main(w, h, cap, capw, caph, fps, flushlate, show=False):
     global filename, start, t, char4im, writing, color, flush
     capw = capw*2
     if 'WT_SESSION' in os.environ:
-        char4im.append("■")
+        #char4im.append("■")
+        pass
     elif color and os.name == "nt":
         capw = capw/2 #Windows Terminalは■の縦横比が2:1、cmdは1:1なため
     if w/capw < h/caph:
@@ -85,21 +86,42 @@ def main(w, h, cap, capw, caph, fps, flushlate, show=False):
     else:
         start = time.perf_counter()
         i = 0
-        t.setDaemon(True)
+        frame_txt = ""
         t.start()
         ret, frame = cap.read()
         if show:
             cv2.imshow("frame", cv2.resize(frame, (w, h*2)))
             cv2.waitKey(1)
         frame_array = numpy.array(cv2.resize(frame, (w, h)), dtype=numpy.uint8)
-        frame_array = 0.299 * frame_array[:, :, 2] + 0.587 * frame_array[:, :, 1] + 0.114 * frame_array[:, :, 0]
-        frame_txt = []
-        for j in range(h):
-            text = ""
-            for k in range(w):
-                text += char4im[int(frame_array[j, k]*(len(char4im))/256)] #frame_array[j, k]の値は255がMAX
-            frame_txt.append(text)
-        print("\n".join(frame_txt))
+        if color:
+            writing = True
+            if (i+1)%flushlate == 0 and flush:
+                print("\033c\n"+frame_txt, end="")
+            frame_txt = ""
+            oldr = 256
+            oldg = 256
+            oldb = 256
+            for j in range(h):
+                text = ""
+                for k in range(w):
+                    b, g, r = frame_array[j, k]
+                    if oldr != r or oldg != g or oldb != b:
+                        text += f"\033[38;2;{r};{g};{b}m"
+                        oldr = r
+                        oldg = g
+                        oldb = b
+                    text += "■"
+                frame_txt += text+"\n"
+            print("\n"+frame_txt[::-1].replace("\n", "", 1)[::-1])
+            lh = h
+        else:
+            frame_array = 0.299 * frame_array[:, :, 2] + 0.587 * frame_array[:, :, 1] + 0.114 * frame_array[:, :, 0]
+            frame_txt = ""
+            for j in range(h):
+                text = ""
+                for k in range(w):
+                    text += char4im[int(frame_array[j, k]*(len(char4im))/256)] #frame_array[j, k]の値は255がMAX
+                    frame_txt += text+"\n"
         now = time.perf_counter()
         if (i+1)/fps < now-start:
             skip = True
@@ -127,12 +149,11 @@ def main(w, h, cap, capw, caph, fps, flushlate, show=False):
                 frame_array = numpy.array(cv2.resize(frame, (w, h)), dtype=numpy.uint8)
                 if color:
                     writing = True
-                    if (i+1)%flushlate == 0 and flush:
-                        print("\033c", end="")
                     frame_txt = ""
                     oldr = 256
                     oldg = 256
                     oldb = 256
+                    print(f"\033[{lh}F", end="")
                     for j in range(h):
                         text = ""
                         for k in range(w):
@@ -143,8 +164,8 @@ def main(w, h, cap, capw, caph, fps, flushlate, show=False):
                                 oldg = g
                                 oldb = b
                             text += "■"
-                        frame_txt += text+"\n"
-                    print("\n"+frame_txt[::-1].replace("\n", "", 1)[::-1], end="\033[0m\n")
+                        print("\033[K"+text)
+                    lh = h
                 else:
                     frame_array = 0.299 * frame_array[:, :, 2] + 0.587 * frame_array[:, :, 1] + 0.114 * frame_array[:, :, 0]
                     frame_txt = ""
@@ -153,7 +174,7 @@ def main(w, h, cap, capw, caph, fps, flushlate, show=False):
                         for k in range(w):
                             text += char4im[int(frame_array[j, k]*(len(char4im))/256)] #frame_array[j, k]の値は255がMAX
                         frame_txt += text+"\n"
-                    print(frame_txt)
+                    os.write(1, ("\n"+frame_txt).encode())
                 writing = False
                 if (i+1)/fps < time.perf_counter()-start:
                     skip = True
@@ -171,6 +192,7 @@ def exitter(hoge, fuga):
     cv2.destroyAllWindows()
     if color:
         os.write(1, b"\033[0m")
+        os.write(1, b"\033[2J")
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     os._exit(0)
 
@@ -181,24 +203,20 @@ color = True
 flush = False
 flushlate = 30
 parser = argparse.ArgumentParser(description="ビデオプレイヤー on ターミナル")
-parser.add_argument("-f", "--filename", type=str, help="動画ファイル名を指定します。")
+parser.add_argument("-f", "--filename", type=str, help="動画ファイル名を指定します。-cオプションを無視します。")
 parser.add_argument("-c", "--camnum", help="使用するカメラの番号を指定します。既定値0", type=int, default=0)
 parser.add_argument("-m", "--mono", help="モノクロで出力します。", action="store_true")
-parser.add_argument("-l", "--flushlate", help="出力を消去する間隔を指定します。(単位:フレーム)", type = int)
 args = parser.parse_args()
 signal.signal(signal.SIGINT, exitter)
 if not args.filename == None:
     cap = cv2.VideoCapture(args.filename)
     try:
-        t = Thread(target=audio_player, args=[VideoFileClip(args.filename)])
+        t = Thread(target=audio_player, args=[VideoFileClip(args.filename)], daemon=True)
     except OSError:
         print("ファイルが開けません。ファイル名を確認してください。")
         exit()
 else:
     cap = cv2.VideoCapture(args.camnum)
-if not args.flushlate == None:
-    flushlate = args.flushlate
-    flush = True
 color = not args.mono
 if not cap.isOpened():
     print("OpenCVの実行に失敗しました。カメラが正しく接続されているか確認してください。")
