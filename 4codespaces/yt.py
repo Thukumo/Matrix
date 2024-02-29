db02d23b97d356eba730554af4979ceb31890fa7
つくも <92912896+Thukumo@users.noreply.github.com>
2024-02-29 06:50:20 +0000

    dd yt.py

--- 4codespaces/yt.py
+++ 4codespaces/yt.py
@@ -1,1 +1,7 @@
-import yt_dlp
+import sys
+from yt_dlp import YoutubeDL
+if len(sys.argv) < 2:
+    print('Usage: yt.py <url>')
+    sys.exit(1)
+with YoutubeDL() as ydl:
+    ydl.download([sys.argv[1]])
